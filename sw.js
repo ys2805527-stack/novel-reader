36768edebc0e2ae8643ebcd85baf7c682b3aeed7
ys2805527-stack <ys2805527-stack@users.noreply.github.com>
2026-08-22 16:49:28 +0000

    fix: 图标文件改名为v2强制刷新iOS PWA图标缓存

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "novel-reader-v20";
+const CACHE = "novel-reader-v21";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-180.png",
-  "./icon-192.png",
-  "./icon-512.png",
+  "./icon-180v2.png",
+  "./icon-192v2.png",
+  "./icon-512v2.png",
 ];
 
 self.addEventListener("install", (e) => {
